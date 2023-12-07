8500e91b5a3efdf83f06bee3eb44e02e8a8dbbc5
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-12-07 16:09:56 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -19,11 +19,6 @@ const User = db.sequelize.define('user',{
         type: db.Sequelize.STRING,
         allowNull: false,
         unique: true
-    },
-    image: {
-        type: db.Sequelize.STRING,
-        allowNull: true,
-        unique: false
     }
 })
 
