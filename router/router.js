2cce7d6f4d98f997d92406e665f6c9e7cb48c4d7
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-12-07 15:37:49 -0400

    Update router.js

--- router/router.js
+++ router/router.js
@@ -5,7 +5,7 @@ const userController = require('../controller/userController');
 
 routes.post('/user', userController.createUser);
 routes.get('/users', userController.findAllUser);
-routes.get('/user', userController.findOneUser);
+routes.get('/user/:id', userController.findOneUser);
 routes.delete('/user/:id', userController.deleteUser);
 routes.put('/user/:id', userController.updateUser);
 routes.post('/user/authenticated', userController.authenticatedUser);
